073710e5af835f3f78e1dfe2f4b6cf268c1cf5e3
Donovan Allen <dallen.928@gmail.com>
2016-10-14 10:56:46 -0400

    adds beginning of game logic

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ $(document).ready(function(){
   var playerOne = {
     name: "",
     score: 0,
-  };
+  }; // this could also be an array?
+
+  var mainWord = "HANGMAN";
+
+  var $letterGuess = $("#letterGuess");
 
   var $button = $("button");
 
@@ -12,16 +16,17 @@ $(document).ready(function(){
 
   var $form = $("form");
 
-  var $play = $("#playGame")
+  var $play = $("#playGame");
 
-  var $name = $()
+  console.log(playerOne);
+  console.log(mainWord);
 
-  console.log(playerOne)
-
+// shows player form
   $button.on("click", function(){
     $form.show();
   });
 
+//gets player's name and greets, logs in obj
   $play.on("click", function(){
     playerOne.name = $nameInput.val();
     $form.hide();
@@ -29,7 +34,46 @@ $(document).ready(function(){
     console.log(playerOne);
   });
 
-  
+//get value of #letterGuess on submit
+// .keypress(function(event) {
+//   if (event.which == 13) {
+//       event.preventDefault();
+//       $("form").submit();
+//   }
+// });
+
+  $letterGuess.keypress(function(evt){
+    if (evt.which == 13){
+      evt.preventDefault();
+      for (var i=0; i < mainWord.length; i++){
+        if ($letterGuess.val() == mainWord.charAt(i)){
+          alert("good guess!")
+        } else {
+          alert ("WRONG!")
+        };
+        // console.log(c);
+      };
+      console.log($letterGuess.val());
+
+    };
+  });
+  //
+  // $letterGuess.keypress(function(evt){
+  //   if (evt.which == 13){
+  //     evt.preventDefault();
+  //   for(var i=0, c=''; c = mainWord.charAt(i); i++)
+  //     console.log(i);
+  //     console.log(c);
+  //
+  //   };
+  // });
+
+
+//iterate through each letter of mainWord and if #letterGuess val = _____, ; else alert("nope, try again")
+
+
+
+
 
 
 
