7874f800fb19b1f00024ee45ed72ee94d53b760a
Donovan Allen <dallen.928@gmail.com>
2016-10-14 16:56:46 -0400

    matching input letter with array

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     score: 0,
   };
 
-  var wordToGuess = ["hangman"];
+  var wordToGuess = [""];
   var word = [];
 
   var mainWord = "hangman";
@@ -41,22 +41,20 @@ $(document).ready(function(){
     alert("Hello, " + playerOne.name + ". You are Player 1.");
     console.log(playerOne);
     wordToGuess = prompt("Enter a word for Player 2 to guess.");
-    console.log(wordToGuess);
-    word = wordToGuess.split("");
-    console.log(word);
-    $gameSpace.show();
-    $.each(word, function(index, value){
-      $("<div />", {
-        'text': value,
-        'class': "hiddenLetter"
-      }).appendTo("#mainWord");
-    });
+    if (wordToGuess != ""){
+      console.log(wordToGuess);
+      word = wordToGuess.split("");
+      console.log(word);
+      $gameSpace.show();
+      $.each(word, function(index, value){
+        $("<div />", {
+          'text': value,
+          'class': "hiddenLetter"
+        }).appendTo("#mainWord");
+      });
+    };
     console.log();
   });
-
-//select a word
-
-//
 
 //on enter, match input with each letter of mainWord
   $letterGuess.keypress(function(evt){
@@ -66,12 +64,8 @@ $(document).ready(function(){
       evt.preventDefault();
       console.log($guess);
 
-      if ($letterGuess.val().length > 1){
-        alert("Please enter one letter at a time!")
-      } else {
-        for (var i=0; i < mainWord.length; i++){
-
-          var letters = mainWord.charAt(i);
+        for (var i=0; i < wordToGuess.length; i++){
+          var letters = wordToGuess.charAt(i);
           console.log(letters);
           var $guess = $letterGuess.val();
           console.log($guess);
@@ -83,12 +77,15 @@ $(document).ready(function(){
           };
 
           if (letters == $guess){
-            alert($guess + " is a good guess")
+            alert($guess + " is a good guess");
+            $letterGuess.val("");
+          } else if (letters != $guess) {
+            alert("Try again");
+            $letterGuess.val("");
           };
 
         };
         console.log($guess);
-      }
 
     };
   });
